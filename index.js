13f561efa61f98f400efc67c93506c4ad51ed950
britdavis <brit@britdavis.com>
2016-10-16 15:22:13 -0600

    Initial Commit
    GET works, POST does not

---
+++ index.js
@@ -0,0 +1,60 @@
+/* jshint esversion:6 */
+
+var http = require("http");
+var fs = require("fs");
+
+var grades = 
+{
+    algebra : "A",
+    "p.e." : "C",
+    english : "B"
+};
+
+var server = http.createServer((req,res) => {
+	if (req.url === "/index.html") {
+		fs.readFile("index.html", (err, data) => {
+			res.write(data);
+			res.end();
+		});
+	} else if(req.url === "/grades") {
+		res.write(JSON.stringify(grades));
+		res.end();
+
+	
+
+	} else if(req.url ==="/schedule" && req.method ==="POST") {
+			var queryData = "";
+
+			req.on('data', function(data) {
+				queryData += data;
+				if(queryData.length > 1e6) {
+					queryData = "";
+					res.writeHead(413, {'Content-Type': 'text/plain'}).end();
+					req.connection.destroy();
+				}
+			});
+
+			req.on('end', function() {
+				// need to change this to add class to grades object
+				//fortunes.push(queryData);
+				// grades.queryData = ""; <-- doesn't work
+			});
+
+		}	else if(req.url === "/schedule") {
+		fs.readFile("schedule.html", (err, data) => {
+			res.write(data);
+			res.write(JSON.stringify(Object.keys(grades)));
+			res.end();
+	});
+		
+
+	
+	} else {
+		res.write("This is Brit's school-api server");
+		res.end();
+	} 
+});
+
+server.listen(8000, () => {
+	console.log("Server started on port 8000");
+});
