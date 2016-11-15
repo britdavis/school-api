b68fffa1f9f97bac9f7d9f785600b028f09415c9
britdavis <brit@britdavis.com>
2016-11-14 17:43:50 -0700

    got homework post working
    thanks sarah

--- index.js
+++ index.js
@@ -19,21 +19,37 @@ var homework =
 };	
 
 var server = http.createServer((req,res) => {
-	if (req.url === "/index.html") {
+	if (req.url === "/" || req.url === "/index.html") {
 		fs.readFile("index.html", (err, data) => {
 			res.write(data);
 			res.end();
 		});
+
+
+
 	} else if(req.url === "/grades") {
 		res.write(JSON.stringify(grades));
 		res.end();
 
-	} else if(req.url === "/homework/test") {
-		// var test = homework.algebra[0];
-		var pathname = url.parse(req.url).pathname.substr(1);
-		console.log(pathname);
-		res.write(JSON.stringify(homework[pathname]));
-		res.end();
+	} else if(req.url === "/homework" && req.method === "POST") {
+		var queryData = "";
+
+			req.on('data', function(data) {
+				queryData += data;
+				if(queryData.length > 1e6) {
+					queryData = "";
+					res.writeHead(413, {'Content-Type': 'text/plain'}).end();
+					req.connection.destroy();
+				}
+			});
+
+			req.on('end', function() {
+				grades[queryData] = "C"; 
+				homework[queryData] = true;
+			res.write("success");
+			res.end();	
+			});
+
 
 	} else if(req.url === "/homework") {
 		res.write(JSON.stringify(homework));
@@ -58,6 +74,8 @@ var server = http.createServer((req,res) => {
 				//fortunes.push(queryData);
 				grades[queryData] = ""; 
 				homework[queryData] = false;
+				res.write("success");
+				res.end();
 			});
 
 	} else if(req.url === "/schedule") {
